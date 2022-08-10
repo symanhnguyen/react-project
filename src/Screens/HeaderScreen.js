65251293871839809f6d4ac36903c35e069a127e
symanhnguyen <63956316+symanhnguyen@users.noreply.github.com>
2022-08-10 20:34:14 +0700

    'project'

---
+++ src/Screens/HeaderScreen.js
@@ -0,0 +1,14 @@
+// import React from "react";
+// import { ReactDOM } from "react";
+
+
+const HeaderScreen = () => {
+  return (
+    <main className="header">
+      {/* <h1 className="text-4xl font-bold">Header</h1> */}
+      {/* <img src="../Image/legana.png" /> */}
+    </main>
+  );
+};
+
+export default HeaderScreen;
